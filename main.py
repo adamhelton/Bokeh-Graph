e3d23b3a0f8ee7a5e6d4d3fe8d150dfba578c7e0
Adam Helton <helton93@protonmail.com>
2019-09-18 12:37:13 -0400

    added smaple data and output file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Make a simple Bokeh line graph
 
 from bokeh.plotting import figure 
+from bokeh.io import output_file, show
+
+#prepare some data
+
+x = [1,2,3,4,5]
+y = [6,7,8,9,10]
+
+#prepare the output file
